5e4a3a295ed3f73bd1ff1da99137791b2636b706
nagarajankola <ankola.nagraj07@gmail.com>
2021-06-20 22:59:48 +0530

    filtering tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,8 +7,22 @@ const Tour = require("./../models/tourModel");
 
 // route to get all tours
 exports.getAllTours = async (req, res) => {
+
   try {
-    const tours = await Tour.find();
+    // BUILDING QUERY
+    const queryObj = {...req.query};
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    const query =  Tour.find(queryObj);
+    // const tour = await Tour.find().where('something').equalTo(ss).where('rfne').equalTo('kejn');
+    
+    
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: "success",
       result: tours.length,
